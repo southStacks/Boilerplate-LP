a489bbeda16d29f615b208f9865cf773d7a268b4
Raoan Marcel <raoanmarceljukowski@gmail.com>
2026-03-01 17:06:15 -0300

    Adição de modo dark e efeito onda para aplicação da cor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'; // O Astro já deve ter adicionado isto
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://suaagencia.com.br', 
+  
+  integrations: [
+    react(), 
+    sitemap(),
+    partytown({
+      // Isso permite que scripts no Partytown enviem dados para a página principal
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
